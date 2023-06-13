a26f5541a5b13c2b7bf85503d13b7bf69533d1b4
Kevin Gao <kevinjamesgao@gmail.com>
2023-06-12 19:48:19 -0700

    added changes to sample app

--- sample/index.php
+++ sample/index.php
@@ -2,89 +2,31 @@
 <html>
 <head>
     <title>Login with Descope</title>
+    <div class="container"></div>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 
-    <script src="https://cdn.auth0.com/js/auth0/9.13/auth0.min.js"></script>
+    <script src="https://unpkg.com/@descope/web-component@latest/dist/index.js"></script>
+    <script src="https://unpkg.com/@descope/web-js-sdk@1.0.0/dist/index.umd.js"></script>
     <script>
-        function createToken(userDetails, sessionToken, redirectURL, projectId) 
-        {
-            var formData = new FormData();
-            formData.append("userId", userDetails.userId);
-            formData.append("userName", userDetails.name);
-            formData.append("sessionToken", sessionToken);
-            formData.append("projectId", projectId);
+        sdk.refresh()
+        const container = document.getElementById('container')
 
-            var xmlHttp = new XMLHttpRequest();
-            let getUrl = window.location;
-            let baseUrl = getUrl.protocol + "//" + getUrl.host;
+        if (!sessionToken) { 
+            container.innerHTML = '<descope-wc project-id="' + projectId + '" flow-id="sign-up-or-in"></descope-wc>'
+            const wcElement = document.getElementsByTagName('descope-wc')[0]
 
-            xmlHttp.onreadystatechange = function () {
-                if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                    window.location = `${baseUrl}/${redirectURL}`;
-                }
-            };
-            xmlHttp.open("post", `${baseUrl}/src/descope-token.php`);
-            xmlHttp.send(formData);
-        }
-
-        const onSuccess = (e) => {
-            const sessionToken = e.detail.sessionJwt;
-
-            createToken(
-                e?.detail?.user,
-                sessionToken,
-                e.target.getAttribute("redirect_url"),
-                e.target.getAttribute("project-id")
-            );
-        };
-
-        const onError = (err) => console.log(err);
-
-        async function inject_flow(projectId, flowId, redirectUrl) {
-            const sdk = Descope({
-                projectId: projectId,
-                persistTokens: true,
-                autoRefresh: true,
-            });
-
-            const refreshToken = sdk.getRefreshToken();
-            const validRefreshToken = refreshToken && !sdk.isJwtExpired(refreshToken);
-            if (validRefreshToken) {
-                sdk.refresh();
-                const sessionToken = sdk.getSessionToken();
-                const user = await sdk.me();
-                createToken(user.data, sessionToken, redirectUrl, projectId);
-            } else {
-                const e = document.getElementById("descope_flow_div");
-                e.innerHTML = `<descope-wc project-id=${projectId} flow-id=${flowId} redirect_url=${redirectUrl}></descope-wc>`;
-                const wcElement = document.getElementsByTagName("descope-wc")[0];
-                if (wcElement) {
-                    wcElement.addEventListener("success", onSuccess);
-                    wcElement.addEventListener("error", onError);
-                }
+            const onSuccess = (e) => {
+                sdk.refresh()
+                window.location.href = "/dashboard"
             }
-        }
 
-        async function logout(projectId, loginPageUrl) {
-            const sdk = Descope({
-                projectId: projectId,
-                persistTokens: true,
-                autoRefresh: true,
-            });
-            
-            const resp = await sdk.logout();
-            
-            // Redirect back to login page
-            window.location = loginPageUrl;
+            const onError = (err) => console.log(err);
+
+            wcElement.addEventListener('success', onSuccess)
+            wcElement.addEventListener('error', onError)
+        } else {
+            window.location.href = "/dashboard"
         }
     </script>
 </head>
-<body class="text-center">
-    <div class="container">
-        <div class="py-5 text-center">
-            <h2>Login with Descope</h2>
-            <div class="descope_flow_div"></div>
-        </div>
-    </div>
-</body>
 </html>
